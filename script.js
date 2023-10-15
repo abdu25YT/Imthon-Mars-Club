3c0ed3bfd26528f4b11e10b160a57a8d3e8d76b8
abdu25 <146803515+abdu25YT@users.noreply.github.com>
2023-10-15 18:42:47 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelector('.p').classList.add('text_horse')
+document.querySelector('.h2').classList.add('text_horse')
+
+
+function visible() {
+    document.querySelector('.p').classList.remove('text_horse')
+    document.querySelector('.h2').classList.remove('text_horse')
+}
+
+function novisible() {
+    document.querySelector('.p').classList.add('text_horse')
+    document.querySelector('.h2').classList.add('text_horse')
+}
